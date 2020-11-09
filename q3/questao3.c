1084073eb19b7468f7be6a12b995ae9d1e9bea66
Ana Albuquerque <51363891+aninhalbuquerque@users.noreply.github.com>
2020-11-09 12:11:14 -0300

    Update questao3.c

--- q3/questao3.c
+++ q3/questao3.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#define n1 15
-#define n2 2
-#define p 5
+
+#define n1 15 //tamanho de s1
+#define n2 2  //tamanho de s2
+#define p 5   //quantidade de threads
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 long qtSub = 0;
@@ -12,15 +13,27 @@ char s1[n1+5], s2[n2+5];
 void *contaSubstring(void * pos){
     long inicio = (long)pos;
     long tam = n1/p + inicio;
-    long cont = 0;
+    long cont = 0; //cada thread tem um contador local para calcular a 
+                   //quantidade de ocorrências de s2 na sua determinada substring de s1
+
+    //para contar a quantidade de substrings, o primeiro loop indica 
+    //qual vai ser a primeira letra a ser conferida com a string s2
     for(long i = inicio ; i < tam ; i++){
         if(s1[i] == s2[0] && i+n2-1 < tam){
             for(long j = 1 ; j < n2 ; j ++){
                 if(s1[i+j] != s2[j]) break;
-                if(j == n2 - 1) cont++;
+                if(j == n2 - 1) cont++; 
+                //caso tenha chegado no final da interação do segundo loop, 
+                //significa que todas as letras são iguais, fazendo com que 
+                //exista uma ocorrência de s2 em s1 começando da posição i, 
+                //então incrementamos no contador local
             }
         }
     }
+
+    //ao checar todas as ocorrências de s2 na substring determinada, 
+    //a thread aciona o mutex para poder adicionar a quantidade 
+    //calculada localmente no contador global
     pthread_mutex_lock(&mutex);
     qtSub += cont;
     pthread_mutex_unlock(&mutex);
@@ -29,9 +42,9 @@ void *contaSubstring(void * pos){
 }
 
 int main(void) {
-    printf("Digite s1 (15 digitos): ");
+    printf("Digite s1 (%d digitos): ", n1);
     scanf("%s", s1);
-    printf("Digite s2 (2 digitos): ");
+    printf("Digite s2 (%d digitos): ", n2);
     scanf("%s", s2);
 
     pthread_t *thread = malloc (p*sizeof(pthread_t));
@@ -42,8 +55,10 @@ int main(void) {
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
+    //Criação das threads
     long tam = n1/p;
-    for (long i = 0 ; i < p ; i++) {   
+    for (long i = 0 ; i < p ; i++) { 
+        //o índice de cada thread vai ser a posição que ela irá começar a checar na string  
         long par = i*tam;
         rc = pthread_create(&thread[i], &attr, contaSubstring, (void *)par);
         if (rc) {
@@ -52,6 +67,7 @@ int main(void) {
         }
     }
     
+    //Espera todas as threads acabarem para poder dar o resultado
     pthread_attr_destroy(&attr);
     for (int i = 0 ; i < p ; i++) {
         rc = pthread_join(thread[i], &status);
