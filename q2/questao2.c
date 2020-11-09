6f5a7ebf8c6086fb4f5fee3a40aab9d6a115dd01
Victória <56272572+vlsc@users.noreply.github.com>
2020-11-09 18:39:17 -0300

    Update questao2.c

--- q2/questao2.c
+++ q2/questao2.c
@@ -5,23 +5,25 @@
 #include <sys/time.h>
 
 int numArquivos, numLinhas;
-//guarda quantos arquivos ficam com cada thread
+//guarda QUANTOS arquivos ficam com cada thread
 int *numF=NULL;
-//guarda quais arquivos ficam com cada thread
+//guarda QUAIS arquivos ficam com cada thread
 int **threadsF=NULL;
 char **placa;
 pthread_mutex_t *mutex;
 pthread_mutex_t print = PTHREAD_MUTEX_INITIALIZER;
+//essas strings farão a tela mudar de cor
 char colors[8][10] = {"\e[41m", "\e[43m", "\e[44m", "\e[45m", "\e[42m", "\e[40m", "\e[46m", "\e[47m"};
 char reset[10] = "\e[0m";
 
+//a função que printa a placa da maneira correta
 void printa(){
   system("clear");
   for(int i=0; i<numLinhas; i++){
   printf("%s" "%s" "%s\n", colors[i], placa[i], reset);
   }
 }
-
+//função que será passada para as threads, para que cada uma possa fazer a leitura de seus arquivos
 void *func(void *id){
   char arquivo[9]="arq0.txt";
   char indice;
@@ -30,29 +32,30 @@ void *func(void *id){
   int tid = *((int *)id); 
   for (int i=0;i<numF[tid];i++){
     if(threadsF[tid][i]){
+      //aqui nós alteramos a string arquivo e colocamos o valor correto no caracter 3, por exemplo: a thread x vai ler o arquivo 1, logo, "arq0.txt" se torna "arq1.txt"
       indice = threadsF[tid][i]+'0';
       arquivo[3]=indice;
       FILE *file = fopen(arquivo,"r");
       if (file == NULL){
         printf("erro ao abrir arquivo %s com a thread %d\n", arquivo, tid);
       }
-      while (!feof(file)){
-        fscanf(file,"%d",&linha);
-        pthread_mutex_lock(&mutex[linha-1]);
-        fscanf(file, " %[^\n]", modif);
+      while (!feof(file)){//aqui a thread lê o arquivo até que ele acabe
+        fscanf(file,"%d",&linha); //salvamos a linha que deverá ser alterada
+        pthread_mutex_lock(&mutex[linha-1]); //travamos o mutex daquela linha
+        fscanf(file, " %[^\n]", modif); //salvamos a modificação que deve ser feita
         placa[linha-1]= (char *)realloc(placa[linha-1], (strlen(modif)+1)*sizeof(char));
-        strcpy(placa[linha-1],modif);
-        threadsF[tid][i]=0;
+        strcpy(placa[linha-1],modif); //fazemos a modificação
+        threadsF[tid][i]=0; //aqui, estamos dizendo que, como o valor da matriz que fala qual arquivo fica com cada thread, será 0, significa que o arquivo já foi lido
         pthread_mutex_lock(&print);
-        printa();
+        printa(); //printamos a placa
         pthread_mutex_unlock(&print);
-        struct timespec timeToWait;
+        struct timespec timeToWait; 
         struct timeval now;
         gettimeofday(&now,NULL);
         timeToWait.tv_sec = now.tv_sec+2;
         do {
           gettimeofday(&now, NULL);
-        } while (now.tv_sec != timeToWait.tv_sec);
+        } while (now.tv_sec != timeToWait.tv_sec); //esperamos o tempo necessário de 2 segundos para que a linha possa ser alterada novamente
         pthread_mutex_unlock(&mutex[linha-1]);
       }
       fclose(file);
@@ -72,7 +75,7 @@ int main(){
   placa = (char **)malloc((numLinhas+1)*sizeof(char *));
   mutex = (pthread_mutex_t *)malloc((numLinhas+1)*sizeof(pthread_mutex_t));
   for(i = 0; i<=numLinhas;i++){
-    pthread_mutex_init(&mutex[i], NULL);
+    pthread_mutex_init(&mutex[i], NULL); //inicializamos os mutexes de cada linha
   }
   pthread_t threads[numThreads];
   int *taskids[numThreads];
@@ -80,8 +83,8 @@ int main(){
   numF = (int *)calloc(numThreads, sizeof(int));
   i=0;
   int j=0;
-  //numero q representa cada arquivo
-  int repArq = 1;
+  int repArq = 1;//numero que representa cada arquivo
+  
   //define quais e quantos arquivos vão pra cada thread
   while(repArq<=numArquivos){
     threadsF[i]=(int *)realloc(threadsF[i],sizeof(int));
@@ -89,7 +92,7 @@ int main(){
     numF[i]++;
     i++;
     if (i>=numThreads) {
-      i=0;
+      i=0; //aqui, se i já chegou ao final da linha da matriz, deverá voltar ao começo e passar para a proxima linha
       j++;
     }
     repArq++;
