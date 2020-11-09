7c1036630c59d7471e189fc960bdfb6f882937bb
Victória <56272572+vlsc@users.noreply.github.com>
2020-11-09 18:29:44 -0300

    Update questao1.c

--- q1/questao1.c
+++ q1/questao1.c
@@ -3,15 +3,19 @@
 #include <stdlib.h>
 #define c 1000000
 
-int cont = 0;
+int cont = 0; //declarando um contador global para ser incrementado até o valor c (1000000)
 pthread_mutex_t mymutex = PTHREAD_MUTEX_INITIALIZER;
 
+//essa é a função que faz o incremento
 void *inc(void *id){
+  //o id da thread é passado como parametro para sabermos qual thread atingiu o valor
   int tid = *((int *)id); 
   while(cont < c){
+    //travando o mutex para que toda a operação seja feita e não ocorram erros pelo escalonamento
     pthread_mutex_lock(&mymutex);
     cont++;
     if (cont == c){
+      //se o contador atinge o valor c, é printada a thread que estava executando
       printf("A thread %d atingiu o valor %d :)\n", tid, cont);
     }
     pthread_mutex_unlock(&mymutex);
@@ -31,7 +35,7 @@ int main(void) {
   int *taskids[n];
   
   for(int t = 0; t < n; t++){
-    //esse mutex eh p garantir que sempre q a main for criar uma thread, ela vai passar pelo if, p saber se o valor ja foi atingido, pq se ja foi, a main termina e n cria mais nenhuma thread
+    //esse mutex é para garantir que sempre que a main for criar uma thread, ela vai passar pelo if, para saber se o valor ja foi atingido, pq se ja foi, a main termina e não cria mais nenhuma thread
     int continua = 1;
     pthread_mutex_lock(&mymutex);
     if (cont >= c){
@@ -41,6 +45,7 @@ int main(void) {
     if(continua == 0){
       break;
     }
+    //aqui ocorre a criação das threads junto com seus respectivos ids
     taskids[t] = (int *) malloc(sizeof(int));
     *taskids[t] = t;
     rc = pthread_create(&threads[t], NULL, inc, (void *)taskids[t]);
